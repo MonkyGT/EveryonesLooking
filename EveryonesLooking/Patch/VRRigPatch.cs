373b8cea47f5098d87bc1e1bfe991709ed471691
MonkyGT <monkygtstudios@gmail.com>
2025-04-19 00:45:33 -0400

    Initial Commit

---
+++ EveryonesLooking/Patch/VRRigPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace HeadInHand
+{
+    [HarmonyPatch(typeof(VRRig), "LateUpdate")]
+    public class VRRigPatch
+    {
+        public static bool enabled;
+
+        public static void Postfix(VRRig __instance)
+        {
+            if (__instance.isLocal) return;
+            
+            if (enabled)
+            {
+                __instance.head.rigTarget.LookAt(GorillaTagger.Instance.headCollider.transform);
+            }
+        }
+    }
+}   
---
+++ EveryonesLooking/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using BepInEx;
+using HarmonyLib;
+
+namespace HeadInHand
+{
+    // I added the description thing so it is compatible with luna's haunted tweaks (FIX FOR THAT ON MY GITHUB)
+    [Description("HauntedModMenu")]
+    [BepInPlugin("monky.everyoneslooking", "Everyones Looking", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        void Start()
+        {
+            Harmony.CreateAndPatchAll(GetType().Assembly, "monky.everyoneslooking");
+        }
+        
+        void OnDisable()
+        {
+            VRRigPatch.enabled=false;
+        }
+
+        void OnEnable()
+        {
+            VRRigPatch.enabled=true;
+        }
+    }
+}
